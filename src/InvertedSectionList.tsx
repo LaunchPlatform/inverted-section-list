cd046b5816b27af36fd3d53bb9147b047bd5d835
Fang-Pen Lin <hello@fangpenlin.com>
2021-10-31 18:43:36 -0700

    Add inverted section list

---
+++ src/InvertedSectionList.tsx
@@ -0,0 +1,213 @@
+import invariant from "invariant";
+import { Component, ComponentType } from "react";
+import {
+  VirtualizedList,
+  SectionListProps,
+  DefaultSectionT,
+} from "react-native";
+import { ItemWithSeparator } from "react-native/Libraries/Lists/VirtualizedSectionList";
+
+export type Props<ItemT, SectionT = DefaultSectionT> = Omit<
+  SectionListProps<ItemT, SectionT>,
+  "inverted" | "invertStickyHeaders"
+>;
+
+// ref: https://github.com/facebook/react-native/blob/6790cf137f73f2d7863911f9115317048c66a6ee/Libraries/Lists/VirtualizeUtils.js#L237-L245
+function defaultKeyExtractor(item: any, index: number): string {
+  if (typeof item === "object" && item?.key != null) {
+    return item.key;
+  }
+  if (typeof item === "object" && item?.id != null) {
+    return item.id;
+  }
+  return String(index);
+}
+
+interface SubExtractorResult<ItemT, SectionT> {
+  readonly section: SectionT;
+  readonly key: string;
+  readonly index: number | null;
+  readonly header?: boolean;
+  readonly leadingSection?: SectionT;
+  readonly trailingSection?: SectionT;
+  readonly leadingItem?: ItemT;
+  readonly trailingItem?: ItemT;
+}
+
+// Mostly from
+// https://github.com/facebook/react-native/blob/6790cf137f73f2d7863911f9115317048c66a6ee/Libraries/Lists/VirtualizedSectionList.js
+// MIT License: https://github.com/facebook/react-native/blob/6790cf137f73f2d7863911f9115317048c66a6ee/LICENSE
+// We only changes the things needed to be done to make inverted section list sticky header works
+class InvertedSectionList<ItemT, SectionT = DefaultSectionT> extends Component<
+  Props<ItemT, SectionT>
+> {
+  private keyExtractor = (item: ItemT, index: number) => {
+    const info = this.subExtractor(index);
+    return (info && info.key) || String(index);
+  };
+
+  private subExtractor(
+    index: number
+  ): SubExtractorResult<ItemT, SectionT> | undefined {
+    let itemIndex = index;
+    const { getItem, getItemCount, keyExtractor, sections } = this.props;
+    for (let i = 0; i < sections.length; i++) {
+      const section = sections[i];
+      const sectionData = section.data;
+      const key = section.key || String(i);
+      const itemCount = getItemCount!(sectionData);
+      itemIndex -= 1; // The section adds an item for the header
+      if (itemIndex >= itemCount + 1) {
+        itemIndex -= itemCount + 1; // The section adds an item for the footer.
+      } else if (itemIndex === -1) {
+        return {
+          section,
+          key: key + ":header",
+          index: null,
+          header: true,
+          trailingSection: sections[i + 1],
+        };
+      } else if (itemIndex === itemCount) {
+        return {
+          section,
+          key: key + ":footer",
+          index: null,
+          header: false,
+          trailingSection: sections[i + 1],
+        };
+      } else {
+        const extractor =
+          section.keyExtractor || keyExtractor || defaultKeyExtractor;
+        return {
+          section,
+          key:
+            key + ":" + extractor(getItem!(sectionData, itemIndex), itemIndex),
+          index: itemIndex,
+          leadingItem: getItem!(sectionData, itemIndex - 1),
+          leadingSection: sections[i - 1],
+          trailingItem: getItem!(sectionData, itemIndex + 1),
+          trailingSection: sections[i + 1],
+        };
+      }
+    }
+  }
+
+  private _getSeparatorComponent(
+    index: number,
+    info?: SubExtractorResult<ItemT, SectionT>,
+    listItemCount: number
+  ): ComponentType<any> | null {
+    info = info || this.subExtractor(index);
+    if (!info) {
+      return null;
+    }
+    const ItemSeparatorComponent =
+      info.section.ItemSeparatorComponent || this.props.ItemSeparatorComponent;
+    const { SectionSeparatorComponent } = this.props;
+    const isLastItemInList = index === listItemCount - 1;
+    const isLastItemInSection =
+      info.index === this.props.getItemCount!(info.section.data) - 1;
+    if (SectionSeparatorComponent && isLastItemInSection) {
+      return SectionSeparatorComponent;
+    }
+    if (ItemSeparatorComponent && !isLastItemInSection && !isLastItemInList) {
+      return ItemSeparatorComponent;
+    }
+    return null;
+  }
+
+  private renderItem =
+    (listItemCount: number) =>
+    ({ item, index }: { item: ItemT; index: number }) => {
+      const info = this.subExtractor(index);
+      if (!info) {
+        return null;
+      }
+      const infoIndex = info.index;
+      if (infoIndex == null) {
+        const { section } = info;
+        if (info.header === true) {
+          const { renderSectionHeader } = this.props;
+          return renderSectionHeader ? renderSectionHeader({ section }) : null;
+        } else {
+          const { renderSectionFooter } = this.props;
+          return renderSectionFooter ? renderSectionFooter({ section }) : null;
+        }
+      } else {
+        const renderItem =
+          (info.section as any).renderItem || this.props.renderItem;
+        const SeparatorComponent = this._getSeparatorComponent(
+          index,
+          info,
+          listItemCount
+        );
+        invariant(renderItem, "no renderItem!");
+        return (
+          <ItemWithSeparator
+            SeparatorComponent={SeparatorComponent}
+            LeadingSeparatorComponent={
+              infoIndex === 0 ? this.props.SectionSeparatorComponent : undefined
+            }
+            cellKey={info.key}
+            index={infoIndex}
+            item={item}
+            leadingItem={info.leadingItem}
+            leadingSection={info.leadingSection}
+            prevCellKey={(this.subExtractor(index - 1) || {}).key}
+            // Callback to provide updateHighlight for this item
+            setSelfHighlightCallback={this._setUpdateHighlightFor}
+            setSelfUpdatePropsCallback={this._setUpdatePropsFor}
+            // Provide child ability to set highlight/updateProps for previous item using prevCellKey
+            updateHighlightFor={this._updateHighlightFor}
+            updatePropsFor={this._updatePropsFor}
+            renderItem={renderItem}
+            section={info.section}
+            trailingItem={info.trailingItem}
+            trailingSection={info.trailingSection}
+            inverted={!!this.props.inverted}
+          />
+        );
+      }
+    };
+
+  render() {
+    const {
+      // don't pass through, rendered with renderItem
+      ItemSeparatorComponent,
+      SectionSeparatorComponent,
+      renderItem: _renderItem,
+      renderSectionFooter,
+      renderSectionHeader,
+      sections: _sections,
+      stickySectionHeadersEnabled,
+      ...passThroughProps
+    } = this.props;
+
+    const listHeaderOffset = this.props.ListHeaderComponent ? 1 : 0;
+
+    const stickyHeaderIndices: Array<number> | undefined = this.props
+      .stickySectionHeadersEnabled
+      ? []
+      : undefined;
+
+    let itemCount = 0;
+    for (const section of this.props.sections) {
+      // Track the section header indices
+      if (stickyHeaderIndices !== undefined) {
+        // Notice: this is different from the original VirtualizedSectionList,
+        //         since we are actually using footer as the header here, so need to + 1
+        //         for the header
+        stickyHeaderIndices.push(itemCount + listHeaderOffset + 1);
+      }
+
+      // Add two for the section header and footer.
+      itemCount += 2;
+      itemCount += this.props.getItemCount!(section.data);
+    }
+    const renderItem = this._renderItem(itemCount);
+
+    return (
+      <VirtualizedList getItem={() => {}} getItemCount={() => {}} inverted />
+    );
+  }
+}
